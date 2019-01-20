a2c59442d0abe1189a274f2d38c8117c94ad092e
Vlael Layug <vlaellayug@yahoo.com.ph>
2019-01-21 05:46:23 +0800

    Mail Settings

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'reserve-eatall@yahoo.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'rea@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Reserve Eat All'),
     ],
 
